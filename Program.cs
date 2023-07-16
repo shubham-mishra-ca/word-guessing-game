aaf91e346c7ef88f2bbce306eb47d6f74f91e4ee
Shubham Mishra <smishra.on.ca@gmail.com>
2023-07-16 13:29:44 -0400

    Learnt about variables and arrays

--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, my name is "+Console.ReadLine()+". I am "+Console.ReadLine()+" years old.");
-    }
+        string sentence1 = "I love coding.";
+        string sentence2 = "I enjoy learning.";
+        Console.WriteLine(sentence1);
+        Console.WriteLine(sentence2);
+
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        Console.WriteLine(sum);
+
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello my name is " + name);
+
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("I am " + age + " years old.");
+
+        string[] names = { "John", "Jane", "Joe" };
+        Console.WriteLine(names[2]);    
+        }
 }
