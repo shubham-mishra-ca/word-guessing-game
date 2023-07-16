8821e396ac35a1add365058b4d5f74fadd76a346
Shubham Mishra <smishra.on.ca@gmail.com>
2023-07-16 15:44:29 -0400

    Implemented game logic.

--- Program.cs
+++ Program.cs
@@ -4,23 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        string sentence1 = "I love coding.";
-        string sentence2 = "I enjoy learning.";
-        Console.WriteLine(sentence1);
-        Console.WriteLine(sentence2);
+        
+           Random rand = new Random();      // Create a new instance of Random class for generating random numbers
+           string[] words = { "apple", "banana", "orange", "pear" };   // Define an array of words to guess
+           string wordToGuess = words[rand.Next(0,words.Length)];  // Randomly select a word from the array
 
-        int num1 = 5;
-        int num2 = 10;
-        int sum = num1 + num2;
-        Console.WriteLine(sum);
+           bool hasWon = false;         // Initialize a flag to track if the player has won
 
-        string name = Console.ReadLine();
-        Console.WriteLine("Hello my name is " + name);
+           int guesses = words.Length/2;        // Calculate the maximum number of guesses allowed
 
-        int age = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("I am " + age + " years old.");
+            // Loop through the guesses
+           for(int i=0; i< guesses; i++)
+           {
+                Console.WriteLine("Guess the word: ");      // Prompt the player to guess the word
 
-        string[] names = { "John", "Jane", "Joe" };
-        Console.WriteLine(names[2]);    
-        }
+                // Display the available words for reference
+                for(int j=0 ; j< words.Length; j++){
+                    Console.WriteLine(words[j] + " ");
+                }
+                
+                // Display the number of remaining tries
+                Console.WriteLine("Tries left: "+ (guesses - i));
+                Console.WriteLine();
+
+                // Read the player's guess from the console
+                string guess = Console.ReadLine();
+
+                // Check if the guess matches the word to be guessed
+                if(guess == wordToGuess){
+                    hasWon = true;          // Set the flag to indicate the player has won and break out of the loop
+                    break;
+                }
+           }
+
+           // Display the result of the game
+            if(hasWon){
+                Console.WriteLine("You guessed the correct word! You Won!!");
+            }else{
+                Console.WriteLine("Sorry, you did not win this time :( .The word was: "+ wordToGuess);
+            }
+           
+    }
 }
